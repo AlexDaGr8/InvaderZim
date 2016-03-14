a3fd9460e0d013ea3acac3c97c5bcaee6e00f8b3
Alex <alexander.hinton@noblis.org>
2016-03-14 09:26:54 -0400

    data

---
+++ data.js
@@ -0,0 +1,198 @@
+
+    var json = {
+      "ContentEncoding":null,
+      "ContentType":null,
+      "Data":[
+        {
+          "Name":"Operations (TSG)",
+          "Value":51,
+          "Children":[
+            {"Name":"On Hold - (Pending Funding)","Value":1},
+            {"Name":"Pending OPS Notification","Value":9},
+            {"Name":"CLIN(s) Ordered - Pending Completion","Value":2},
+            {"Name":"Awaiting Harris Proposal by Contracts Team","Value":16},
+            {"Name":"Action Required by COR/Williams - Second Pass.","Value":7},
+            {"Name":"Harris Implementation","Value":7},
+            {"Name":"Awaiting Harris Document","Value":2},
+            {"Name":"Action Required by COR/Williams","Value":4},
+            {"Name":"Extension Uploaded. COR Approval of Date Needed by COR/Williams","Value":1},
+            {"Name":"Action Required by TNOM","Value":2}
+          ]
+        },
+        {
+          "Name":"FS-21",
+          "Value":3,
+          "Children":[
+            {"Name":"CLIN(s) Ordered - Pending Completion","Value":1},
+            {"Name":"Awaiting Technical Evaluation","Value":2}
+          ]
+        },
+        {
+          "Name":"Disconnect",
+          "Value":32,
+          "Children":[
+            {"Name":"Pending Equipment Removal Confirmation","Value":32}
+          ]
+        },
+        {
+          "Name":"Enterprise Architecture",
+          "Value":14,
+          "Children":[
+            {"Name":"Pending Invoice","Value":1},
+            {"Name":"Awaiting NEMS CDRL","Value":4},
+            {"Name":"Action Required by Jim Robb/Ray Goulet/Tim Sienrukos - Second Pass.","Value":1},
+            {"Name":"Awaiting Technical Evaluation","Value":1},
+            {"Name":"Awaiting Harris Proposal by Contracts Team","Value":1},
+            {"Name":"Awaiting Technical Evaluation - In Progress","Value":1},
+            {"Name":"Action Required by Jim Robb/Ray Goulet/Tim Sienrukos","Value":1},
+            {"Name":"Action Required by COR/Williams","Value":4}
+          ]
+        },
+        {
+          "Name":"Other (FTI PO use only)",
+          "Value":39,
+          "Children":[
+            {"Name":"Harris Implementation","Value":1},
+            {"Name":"No Further Action Required","Value":33},
+            {"Name":"Awaiting CDRL Report","Value":1},
+            {"Name":"Harris Implementation w/Proposal Pending","Value":1},
+            {"Name":"Awaiting Harris Proposal by Contracts Team","Value":1},
+            {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1},
+            {"Name":"Action Required by COR/Williams","Value":1}
+          ]
+        },
+        {
+          "Name":"Mission Support",
+          "Value":8,
+          "Children":[
+            {"Name":"Pending OPS Notification","Value":4},
+            {"Name":"Awaiting Harris Document","Value":1},
+            {"Name":"No Further Action Required","Value":2},
+            {"Name":"Action Required by COR (Gloria Richmond) - Second Pass.","Value":1}
+          ]
+        },
+        {
+          "Name":"Regional",
+          "Value":5,
+          "Children":[
+            {"Name":"CLIN(s) Ordered - Pending Completion","Value":2},
+            {"Name":"Action Required by TNOM - Second Pass.","Value":1},
+            {"Name":"Awaiting Technical Evaluation","Value":1},
+            {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1}
+          ]
+        },
+        {
+          "Name":"National Programs - Defense",
+          "Value":2,
+          "Children":[
+            {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1},
+            {"Name":"Site Survey Uploaded. Tech Evaluation Needed","Value":1}
+          ]
+        },
+        {
+          "Name":"National Programs - Terminal",
+          "Value":3,
+          "Children":[
+            {"Name":"Extension Uploaded. COR Approval of Date Needed by COR/Williams","Value":1},
+            {"Name":"CLIN(s) Ordered - Pending Completion","Value":1},
+            {"Name":"Awaiting Site Survey","Value":1}
+          ]
+        },
+        {
+          "Name":"Security",
+          "Value":3,
+          "Children":[
+            {"Name":"Awaiting Harris Document","Value":1},
+            {"Name":"Awaiting Technical Evaluation","Value":1},
+            {"Name":"Action Required by Jenny Perez - Second Pass.","Value":1}
+          ]
+        },
+        {
+          "Name":"National Programs - International",
+          "Value":1,
+          "Children":[
+            {"Name":"Harris Implementation w/Proposal Pending","Value":1}
+          ]
+        },
+        {
+          "Name":"Product Support",
+          "Value":2,
+          "Children":[
+            {"Name":"On Hold - (More FAA Information Requested)","Value":1},
+            {"Name":"On Hold - (PMO Review)","Value":1}
+          ]
+        },
+        {
+          "Name":"FTI SAT",
+          "Value":1,
+          "Children":[
+            {"Name":"Harris Implementation","Value":1}
+          ]
+        },
+        {
+          "Name":"Alaska",
+          "Value":4,
+          "Children":[
+            {"Name":"a","Value":1},
+            {"Name":"b","Value":1},
+            {"Name":"c","Value":2},
+            {"Name":"d","Value":1},
+            {"Name":"e","Value":1},
+            {"Name":"f","Value":2},
+            {"Name":"g","Value":1},
+            {"Name":"h","Value":1},
+            {"Name":"i","Value":2},
+            {"Name":"j","Value":1},
+            {"Name":"k","Value":1},
+            {"Name":"l","Value":2},
+            {"Name":"m","Value":1},
+            {"Name":"n","Value":1},
+            {"Name":"o","Value":2},
+            {"Name":"p","Value":1},
+            {"Name":"q","Value":1},
+            {"Name":"r","Value":2},
+            {"Name":"s","Value":1},
+            {"Name":"t","Value":1},
+            {"Name":"u","Value":2},
+            {"Name":"v","Value":1},
+            {"Name":"w","Value":1},
+            {"Name":"x","Value":2},
+            {"Name":"y","Value":1},
+            {"Name":"z","Value":1},
+            {"Name":"aa","Value":2},
+            {"Name":"bb","Value":1},
+            {"Name":"cc","Value":1},
+            {"Name":"dd","Value":2},
+          ]
+        },
+        {
+          "Name":"Alex's Wonderland",
+          "Value":57,
+          "Children":[
+            {"Name":"a","Value":1,"NameVal":0},
+            {"Name":"b","Value":1},
+            {"Name":"c","Value":1},
+            {"Name":"d","Value":1},
+            {"Name":"e","Value":1},
+            {"Name":"f","Value":1},
+            {"Name":"g","Value":1},
+            {"Name":"h","Value":1},
+
+            {"Name":"i","Value":2},
+            {"Name":"j","Value":2},
+
+            {"Name":"k","Value":3},
+            {"Name":"l","Value":3},
+            {"Name":"m","Value":3},
+            {"Name":"n","Value":3},
+
+            {"Name":"o","Value":4},
+            {"Name":"p","Value":7},
+            {"Name":"q","Value":9},
+            {"Name":"r","Value":13},
+          ]
+        }
+      ],
+      "JsonRequestBehavior":1,
+      "MaxJsonLength":null,
+      "RecursionLimit":null};
---
+++ data/data2.js
@@ -0,0 +1,131 @@
+var json = {
+  "ContentEncoding":null,
+  "ContentType":null,
+  "Data":[
+    {"Name":"Alaska","Value":14,"Children":[
+      {"Name":"Request for Revised Due Date Uploaded. COR Confirmation of Denial of Date Needed.","Value":1,"NameVal":0},
+      {"Name":"CDRL Uploaded. FAA Approval Needed by Teresa Matos/Carl Stokes","Value":1,"NameVal":1},
+      {"Name":"Site Survey Tech Eval Done. FAA Approval Needed","Value":1,"NameVal":2},
+      {"Name":"Harris Implementation","Value":1,"NameVal":3},
+      {"Name":"Action Required by Teresa Matos/Carl Stokes \u003eAlt:Steve Murphy - Phase 2","Value":3,"NameVal":4},
+      {"Name":"Harris Implementation w/Proposal Pending","Value":1,"NameVal":5},{"Name":"Action Required by Anne Suissa - Phase 1","Value":2,"NameVal":6},
+      {"Name":"Action Required by Teresa Matos - Phase 1","Value":3,"NameVal":7},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1,"NameVal":8}]
+    },
+    {"Name":"Operations (TSG)","Value":57,"Children":[
+      {"Name":"On Hold - (Security Review)","Value":1,"NameVal":9},
+      {"Name":"Pending OPS Notification","Value":9,"NameVal":10},
+      {"Name":"CLIN(s) Ordered - Pending Completion","Value":2,"NameVal":11},
+      {"Name":"Awaiting Harris Proposal by Contracts Team","Value":13,"NameVal":12},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":3,"NameVal":8},
+      {"Name":"Harris Implementation","Value":7,"NameVal":3},
+      {"Name":"Awaiting FAA Denial Letter or Document by Contracts Team","Value":3,"NameVal":13},
+      {"Name":"Awaiting Technical Evaluation","Value":3,"NameVal":14},
+      {"Name":"Action Required by Steve Murphy/Mike Wuenstel \u003eAlt:Carl Stokes - Phase 2","Value":1,"NameVal":15},
+      {"Name":"Awaiting Harris Document","Value":2,"NameVal":16},
+      {"Name":"On Hold - (Pending Further Technical Review)","Value":1,"NameVal":17},
+      {"Name":"Request for Revised Due Date Uploaded. FAA Approval of Revised Due Date Needed. by TNOM","Value":1,"NameVal":18},
+      {"Name":"Extension Uploaded. FAA Approval of Date Needed. by TNOM","Value":1,"NameVal":19},
+      {"Name":"Action Required by TNOM \u003eAlt:Ops Manager - Phase 2","Value":3,"NameVal":20},
+      {"Name":"Action Required by TNOM \u003eAlt:Ops Manager - Phase 1","Value":4,"NameVal":21},
+      {"Name":"Action Required by David Meusel/Dave Fischer - Phase 1","Value":1,"NameVal":22},
+      {"Name":"On Hold - (Pending Regional Proposal)","Value":1,"NameVal":23},
+      {"Name":"Not Approved./Declined Harris Request/Proposal.","Value":1,"NameVal":24}]
+    },
+    {"Name":"Other (FTI PO use only)","Value":47,"Children":[
+      {"Name":"Awaiting Technical Evaluation","Value":1,"NameVal":14},
+      {"Name":"Harris Implementation","Value":1,"NameVal":3},
+      {"Name":"No Further Action Required","Value":33,"NameVal":25},
+      {"Name":"Awaiting CDRL Report","Value":1,"NameVal":26},
+      {"Name":"Harris Implementation w/Proposal Pending","Value":1,"NameVal":5},
+      {"Name":"Awaiting Harris Proposal by Contracts Team","Value":1,"NameVal":12},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":3,"NameVal":8},
+      {"Name":"Action Required by COR - Phase 1","Value":4,"NameVal":27},
+      {"Name":"Extension Uploaded. COR Approval of Date Needed by COR","Value":1,"NameVal":28},
+      {"Name":"Action Required by Andy Isaksen - Phase 1","Value":1,"NameVal":29}]
+    },
+    {"Name":"FS-21","Value":6,"Children":[
+      {"Name":"CLIN(s) Ordered - Pending Completion","Value":1,"NameVal":11},
+      {"Name":"Action Required by Teresa Matos/Carl Stokes \u003eAlt:Steve Murphy - Phase 2","Value":1,"NameVal":4},
+      {"Name":"Awaiting Technical Evaluation","Value":1,"NameVal":14},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":3,"NameVal":8}]
+    },
+    {"Name":"Disconnect","Value":32,"Children":[
+      {"Name":"Pending Equipment Removal Confirmation","Value":32,"NameVal":30}]
+    },
+    {"Name":"Enterprise Architecture","Value":21,"Children":[
+      {"Name":"Pending Invoice","Value":1,"NameVal":31},
+      {"Name":"Awaiting NEMS CDRL","Value":4,"NameVal":32},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":9,"NameVal":8},
+      {"Name":"Awaiting Technical Evaluation","Value":1,"NameVal":14},
+      {"Name":"Awaiting Harris Proposal by Contracts Team","Value":1,"NameVal":12},
+      {"Name":"Awaiting Technical Evaluation - In Progress","Value":1,"NameVal":33},
+      {"Name":"Action Required by COR - Phase 1","Value":1,"NameVal":27},
+      {"Name":"Action Required by Teresa Matos/Carl Stokes \u003eAlt:Steve Murphy - Phase 1","Value":1,"NameVal":34},
+      {"Name":"Action Required by Jeri Groce/Ray Goulet/Tim Sienrukos - Phase 1","Value":1,"NameVal":36},
+      {"Name":"Awaiting Harris CDRL by Contracts Team","Value":1,"NameVal":37}]
+    },
+    {"Name":"Mission Support","Value":14,"Children":[
+      {"Name":"Pending OPS Notification","Value":3,"NameVal":10},
+      {"Name":"Awaiting Harris Document","Value":1,"NameVal":16},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":4,"NameVal":8},
+      {"Name":"No Further Action Required","Value":1,"NameVal":25},
+      {"Name":"Action Required by COR (Gloria Richmond) - Phase 2","Value":4,"NameVal":38},
+      {"Name":"Awaiting Harris Proposal by Contracts Team","Value":1,"NameVal":12}]
+    },
+    {"Name":"Regional","Value":10,"Children":[
+      {"Name":"CLIN(s) Ordered - Pending Completion","Value":2,"NameVal":11},
+      {"Name":"Action Required by TNOM - Phase 2","Value":1,"NameVal":39},
+      {"Name":"Awaiting Technical Evaluation","Value":1,"NameVal":14},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":3,"NameVal":8},
+      {"Name":"Action Required by Steve Murphy/Mike Wuenstel \u003eAlt:Carl Stokes - Phase 1","Value":2,"NameVal":40},
+      {"Name":"Action Required by TNOM \u003eAlt:Ops Manager - Phase 1","Value":1,"NameVal":21}]
+    },
+    {"Name":"National Programs - Defense","Value":4,"Children":[
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":3,"NameVal":8},
+      {"Name":"Site Survey Uploaded. Tech Evaluation Needed","Value":1,"NameVal":41}]
+    },
+    {"Name":"National Programs - Terminal","Value":5,"Children":[
+      {"Name":"Awaiting FAA Denial Letter or Document by Contracts Team","Value":1,"NameVal":13},
+      {"Name":"CLIN(s) Ordered - Pending Completion","Value":1,"NameVal":11},
+      {"Name":"Awaiting Site Survey","Value":1,"NameVal":42},
+      {"Name":"Action Required by Steve Murphy/Mike Wuenstel \u003eAlt:Carl Stokes - Phase 2","Value":1,"NameVal":15},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1,"NameVal":8}]
+    },
+    {"Name":"Security","Value":7,"Children":[
+      {"Name":"Awaiting Harris Document","Value":1,"NameVal":16},
+      {"Name":"Awaiting Technical Evaluation","Value":1,"NameVal":14},
+      {"Name":"Action Required by Jenny Perez - Phase 2","Value":3,"NameVal":43},
+      {"Name":"Action Required by Jenny Perez - Phase 1","Value":1,"NameVal":44},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1,"NameVal":8}]
+    },
+    {"Name":"National Programs - International","Value":3,"Children":[
+      {"Name":"Harris Implementation w/Proposal Pending","Value":1,"NameVal":5},
+      {"Name":"Awaiting Technical Evaluation","Value":1,"NameVal":14},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1,"NameVal":8}]
+    },
+    {"Name":"Product Support","Value":4,"Children":[
+      {"Name":"Action Required by Andy Isaksen - Phase 2","Value":1,"NameVal":45},
+      {"Name":"On Hold - (PMO Review)","Value":1,"NameVal":46},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":2,"NameVal":8}]
+    },
+    {"Name":"FTI SAT","Value":3,"Children":[
+      {"Name":"Site Survey Uploaded. Tech Evaluation Needed","Value":1,"NameVal":41},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":2,"NameVal":8}]
+    },
+    {"Name":"FTI Microwave","Value":6,"Children":[
+      {"Name":"Action Required by Dan Chess \u003eAlt:Ops Manager - Phase 1","Value":3,"NameVal":47},
+      {"Name":"Action Required by Dan Chess - Phase 2","Value":1,"NameVal":48},
+      {"Name":"Action Required by COR - Phase 2","Value":1,"NameVal":49},
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":1,"NameVal":8}]
+    },
+    {"Name":"National Programs - En Route","Value":2,"Children":[
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":2,"NameVal":8}]
+    },
+    {"Name":"National Programs - NWS","Value":2,"Children":[
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":2,"NameVal":8}]
+    },
+    {"Name":"NA/OPS Waiver","Value":2,"Children":[
+      {"Name":"Awaiting FAA Letter or Document by Contracts Team","Value":2,"NameVal":8}]
+    }],
+    "JsonRequestBehavior":1,"MaxJsonLength":null,"RecursionLimit":null};
